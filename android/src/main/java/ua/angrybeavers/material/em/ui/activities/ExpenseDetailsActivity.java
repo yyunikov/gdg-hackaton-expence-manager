24b9f72ea26b46147fc6ebf1ad25b7eae218d18a
Vasyl Pirus <vasyl.pirus@gmail.com>
2014-10-24 13:36:12 +0300

    show expense details activity animation

--- android/src/main/java/ua/angrybeavers/material/em/ui/activities/ExpenseDetailsActivity.java
+++ android/src/main/java/ua/angrybeavers/material/em/ui/activities/ExpenseDetailsActivity.java
@@ -1,17 +1,54 @@
 package ua.angrybeavers.material.em.ui.activities;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.view.MenuItem;
 
 import ua.angrybeavers.material.em.R;
 
 /**
  * Created by vasylpirus on 24.10.14.
  */
-public class ExpenseDetailsActivity extends BaseActivity {
+public class ExpenseDetailsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setEnterTransition(new Explode());
+        getWindow().setExitTransition(new Explode());
         setContentView(R.layout.expense_details_layout);
+
+        setupActionBar();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home)
+        {
+            final Intent intent = new Intent(this, AccountActivity.class).
+                    addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    protected void setupActionBar() {
+        final ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setTitle(R.string.app_name);
+        }
+    }
+
+
+
+
 }
