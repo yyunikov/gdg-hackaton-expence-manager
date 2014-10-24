66e4d826c777a96b59508065cb88b0d8f5ecf7a1
Vasyl Pirus <vasyl.pirus@gmail.com>
2014-10-24 13:08:16 +0300

    added expense_details layout

---
+++ android/src/main/java/ua/angrybeavers/material/em/ui/activities/ExpenseDetailsActivity.java
@@ -0,0 +1,17 @@
+package ua.angrybeavers.material.em.ui.activities;
+
+import android.os.Bundle;
+
+import ua.angrybeavers.material.em.R;
+
+/**
+ * Created by vasylpirus on 24.10.14.
+ */
+public class ExpenseDetailsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.expense_details_layout);
+    }
+}
